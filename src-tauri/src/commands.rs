f878b01ac00f9427bdae93becdec00752ca74b51
kevin <zkevin1997@outlook.com>
2023-11-26 17:30:20 +0800

    mod: dep

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,10 +1,12 @@
 use brotli2::read::BrotliDecoder;
-use macos_accessibility_client::accessibility;
 use mouse_position::mouse_position::Mouse;
 use rdev::{simulate, EventType, Key};
 use std::io::Read;
 use std::{thread, time};
 use whatlang::detect;
+
+#[cfg(target_os = "macos")]
+use macos_accessibility_client::accessibility;
 
 fn send(event_type: &EventType) {
     let delay = time::Duration::from_millis(20);
@@ -42,22 +44,26 @@ pub async fn get_text_lang(text: String) -> Result<String, ()> {
 
 #[tauri::command]
 pub async fn auto_copy() {
-    let trusted = accessibility::application_is_trusted_with_prompt();
-    if trusted {
+    #[cfg(target_os = "macos")]
+    {
+        let trusted = accessibility::application_is_trusted_with_prompt();
+        if trusted {
+            thread::sleep(time::Duration::from_millis(400));
+
+            send(&EventType::KeyPress(Key::MetaLeft));
+            send(&EventType::KeyPress(Key::KeyC));
+            send(&EventType::KeyRelease(Key::KeyC));
+            send(&EventType::KeyRelease(Key::MetaLeft));
+        }
+    }
+
+    #[cfg(target_os = "windows")]
+    {
         thread::sleep(time::Duration::from_millis(400));
-
-        #[cfg(target_os = "windows")]
         send(&EventType::KeyPress(Key::ControlLeft));
-        #[cfg(target_os = "macos")]
-        send(&EventType::KeyPress(Key::MetaLeft));
-
         send(&EventType::KeyPress(Key::KeyC));
         send(&EventType::KeyRelease(Key::KeyC));
-
-        #[cfg(target_os = "windows")]
         send(&EventType::KeyRelease(Key::ControlLeft));
-        #[cfg(target_os = "macos")]
-        send(&EventType::KeyRelease(Key::MetaLeft));
     }
 }
 
